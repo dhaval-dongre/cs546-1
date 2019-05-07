6d0907b134fe51712cd66f4e9b3eb7d744494b04
Jack Bittker <jbittker@stevens.edu>
2019-05-07 00:45:00 -0400

    about routes

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -2,11 +2,13 @@ const landingpageRoutes = require("./landingpage");
 const homepageRoutes = require("./homepage");
 const userRoutes = require("./user");
 const bookRoutes = require("./book");
+const aboutRoutes = require("./about");
 const constructorMethod = app => {
   app.use("/homepage", homepageRoutes); 
   app.use("/", landingpageRoutes);
   app.use("/user", userRoutes);
   app.use("/book", bookRoutes);
+    app.use("/book", aboutRoutes);
   app.use("*", (req, res) => {
     res.status(500).json();
     return;
